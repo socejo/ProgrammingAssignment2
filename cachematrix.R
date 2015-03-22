2f4dafe4f75a51893b19e6a05e303bb73b57f4ef
Salvador Ocejo <socejo@icloud.com>
2015-03-22 07:00:08 -0600

    Typos correction on english version

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,21 @@
 
 ## Two functions are required:
 
-## a) one that sets up the calculation "structure" for the inverse matrix, stroing it in cache and 
-## b) one that estracts the inverse matrix from cache OR calculates the inverse and stores it in cache,
-##    dependint on wheter or not it has been previously calculated
+## a) sets up the calculation "structure" for the inverse matrix and for storing it in cache and 
+## b) extracts the inverse matrix from cache OR calculates the inverse and stores it in cache,
+##    depending on whether or not it has been previously calculated
 
 ## This file contains the complete solution i.e.: both functions
 
-## Calling secuence 
+## Calling sequence to validate
 
-##             v1 <- makeCachematrix(originalmatrix)
-##             v2 <- cacheSolve(V1)
-##             v1 %*% v2 should give an identity matrix     
+##             m1 <- originalmatrix
+##             v  <- makeCacheMatrix(m1)
+##             m2 <- cacheSolve(v)
+##             m1 %*% m2 should give an identity matrix     
 
 ## makeCacheMatrix 
-##           Sets up the necesary struicture including calculation of the inverse 
+##           Sets up the necessary structure including calculation of the inverse 
 
 makeCacheMatrix <- function(matriz = matrix()) {
 
@@ -28,21 +29,19 @@ makeCacheMatrix <- function(matriz = matrix()) {
 		creada <<- NULL
 		}
 
-	get <- function() matriz 		## obtain the matriz
+	get <- function() matriz 		## obtain the matrix
 
-	setinverse <- function(solve) creada <<- solve  ## inverse calculation 
+    
+	setinverse <- function(solve) creada <<- solve  ## inverse calculation and storage
  
 	getinverse <- function() creada   ## get inverse matrix  
 
-   list (set=set, get=get, setinverse=setinverse, getinverse=getinverse) ## regresa the vector with the functions and original matrix
+   list (set=set, get=get, setinverse=setinverse, getinverse=getinverse) ## returns the vector with the functions and original matrix
    
 }
 
 ## cacheSolve 
-##        perform inverse matrix calculation if it is not in cache. Otherwise recover from cache
-
-## Eventhough this structure ois very interesting, you finish with two copies of the matrix in memory 
-## which can be a problem if youy don't have big memory available
+##        perform inverse matrix calculation if it is not in cache. Otherwise, recover from cache
 
 cacheSolve <- function(vector, ...) {
 
@@ -50,14 +49,14 @@ cacheSolve <- function(vector, ...) {
 
 	if(!is.null(creada)) {   ## if inverse already calculated  
 		message("getting inverse matrix from cache") 
-		return(creada)      ## get it from cache and return result
+		return(creada)      ## get it from cache and return it as result
 		}
 	
 	datos <- vector$get() 		## get original matrix  
 	
-	creada <- solve(datos,...) 	## determine the inverse  
+	creada <- solve(datos, ...) 	## determine the inverse  
 	
-	vector$setinverse(creada)	## and storew it in cache 
+	vector$setinverse(creada)	## and store it in cache 
 
 	creada					##return inverse matrix as the result
 }
